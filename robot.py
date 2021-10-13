b9cbc155fc2d0ea565291689af7378dd8c469926
Sonya Brazell <sonyabrazell@Sonyas-iMac.local>
2021-10-13 10:45:41 -0500

    Class and method added for robot file. Weapon class edited to add parameter.

---
+++ robot.py
@@ -0,0 +1,8 @@
+class Robot:
+    def __init__(self, name):
+        self.name = ''
+        self.heath = int
+        self.weapon = []
+
+    def attack(self, dinosaur):
+        pass
--- weapon.py
+++ weapon.py
@@ -1,4 +1,4 @@
 class Weapon:
-    def __init__(self):
+    def __init__(self, name, attack_power):
         self.name = ''
         self.attack_power = int
