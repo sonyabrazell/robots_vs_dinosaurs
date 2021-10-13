3f9e49763f7e1a66886b759fba367d741b25d601
Sonya Brazell <sonyabrazell@Sonyas-iMac.local>
2021-10-13 16:05:08 -0500

    Reworked attack functions under Robot and Dinosaur class.

--- dinosaur.py
+++ dinosaur.py
@@ -1,5 +1,3 @@
-from herd import Herd
-from fleet import Fleet
 from robot import Robot
 
 class Dinosaur:
@@ -9,7 +7,7 @@ class Dinosaur:
         self.health = 10
 
     def attack(self, robot):
-        robot_health = Robot.health
-        dinosaur_power = Herd.attack_power
+        robot_health = robot.health
+        dinosaur_power = self.attack_power
         remaining_health = robot_health - dinosaur_power
-        
+        print('After dinosaur attack, the robot now has ',remaining_health,' health remaining.')
--- robot.py
+++ robot.py
@@ -1,20 +1,14 @@
-from fleet import Fleet
-from herd import Herd
 from weapon import Weapon
 from dinosaur import Dinosaur
 
 class Robot:
     def __init__(self, name, weapon):
         self.name = name
-        self.heath = 10
+        self.health = 10
         self.weapon = weapon
 
     def attack(self, dinosaur):
-        #need to have robot attack dinosaur and calculate what the dinosaurs new health score
-        #after subracting the attacking robot's attack score. does not need to specify which robot
-        #or which dinosaur. information can be called in battle.
-        dinosaur = Dinosaur.name
-        dinosaur_health = Dinosaur.health
+        dinosaur_health = dinosaur.health
         weapon_strength = Weapon.attack_power
         remaining_health = dinosaur_health - weapon_strength
         print('After robot attack, the dinosaur now has ',remaining_health,' health remaining.')
