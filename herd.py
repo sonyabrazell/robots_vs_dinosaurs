fd4cfe7973d1b233509ed4904a393521aaa5b569
Sonya Brazell <sonyabrazell@Sonyas-iMac.local>
2021-10-13 13:08:22 -0500

    Created dinosaur objects and added to list in Herd.

--- herd.py
+++ herd.py
@@ -3,4 +3,9 @@ class Herd:
         self.dinosaurs = []
 
     def create_herd(self):
-        pass
+        stegosaurus = Dinosaur('James', 3)
+        triceratops = Dinosaur('Lily', 3)
+        brontosaurus = Dinosaur('Harry', 2)
+        self.dinosaurs.append(stegosaurus)
+        self.dinosaurs.append(triceratops)
+        self.dinosaurs.append(brontosaurus)
