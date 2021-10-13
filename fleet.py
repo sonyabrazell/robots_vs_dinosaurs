c6081a65b56fd6ac77d73c4e53c5f9868f1df74a
Sonya Brazell <sonyabrazell@Sonyas-iMac.local>
2021-10-13 13:05:56 -0500

    Created robot objeects and added them to robot list in fleet.

--- fleet.py
+++ fleet.py
@@ -1,6 +1,13 @@
+from robot import Robot
+
 class Fleet:
     def __init__(self,name):
         self.robots = []
 
     def create_fleet(self):
-        pass
+        robot1 = Robot('Lucius')
+        robot2 = Robot('Narcissa')
+        robot3 = Robot('Draco')
+        self.robots.append(robot1)
+        self.robots.append(robot2)
+        self.robots.append(robot3)
