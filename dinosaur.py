a808e50eee73be65ecafaee5ecd70f3af15ef457
Sonya Brazell <sonyabrazell@Sonyas-iMac.local>
2021-10-13 10:57:28 -0500

    Class and method added to dinosaur file.

---
+++ dinosaur.py
@@ -0,0 +1,8 @@
+class Dinosaur:
+    def __init__(self, name, attack_power):
+        self.name = ''
+        self.attack_power = int
+        self.health = int
+
+    def attack(self, robot):
+        pass
